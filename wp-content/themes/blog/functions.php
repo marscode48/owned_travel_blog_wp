67beba6bf713599321556e9c995a700c5f86b2f0
Adachi Masakazu <marscode48@gmail.com>
2022-01-15 14:34:26 +0900

    styleの読み込みを変更

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -2,6 +2,6 @@
 
 function my_enqueue_styles() {
   wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
-  wp_enqueue_style('style', get_stylesheet_uri(), array('ress'), false, 'all');
+  wp_enqueue_style('main-style', get_stylesheet_uri(), array('ress'), false, 'all');
 }
 add_action('wp_enqueue_scripts', 'my_enqueue_styles');
