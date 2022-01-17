4c34ec8a0ccb074207daf29e586e2e024af67e73
Adachi Masakazu <marscode48@gmail.com>
2022-01-17 14:52:02 +0900

    アイキャッチ画像を有効化

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -1,7 +1,11 @@
 <?php
 
+
 function my_enqueue_styles() {
   wp_enqueue_style('ress', '//unpkg.com/ress/dist/ress.min.css', array(), false, 'all');
   wp_enqueue_style('main-style', get_stylesheet_uri(), array('ress'), false, 'all');
 }
 add_action('wp_enqueue_scripts', 'my_enqueue_styles');
+
+/*アイキャッチ画像の有効化 */
+add_theme_support('post-thumbnails');
