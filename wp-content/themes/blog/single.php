bef54dcf5851077deb48e75c9c95450c85bf11af
Adachi Masakazu <marscode48@gmail.com>
2022-01-19 17:19:38 +0900

    singleのstyleを修正

--- wp-content/themes/blog/single.php
+++ wp-content/themes/blog/single.php
@@ -6,7 +6,7 @@
       <?php
        $cat = get_the_category();
        $catname = $cat[0]->cat_name;
-     ?>
+      ?>
       <article class="main__article">
         <h1 class="main__title">
           <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
