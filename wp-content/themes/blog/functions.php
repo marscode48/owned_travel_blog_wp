95d56fb6f6b8208c8dc46bec3e2ed0be0be5d17e
Adachi Masakazu <marscode48@gmail.com>
2022-01-19 10:45:40 +0900

    functionsのsidebarを作成

--- wp-content/themes/blog/functions.php
+++ wp-content/themes/blog/functions.php
@@ -57,3 +57,16 @@ function pagination($pages = '', $range = 2) {
     echo '</div>';
   }
 }
+
+// ウィジェットエリア（サイドバー）
+if (function_exists('register_sidebar')) {
+  register_sidebar(array(
+    'name' => 'サイドバー',
+    'id' => 'sidebar',
+    'description' => 'サイドバーウィジェット',
+    'before_widget' => '<div>',
+    'after_widget' => '</div>',
+    'before_title' => '<h3 class="side-title">',
+    'after_title' => '</h3>'
+  ));
+}
