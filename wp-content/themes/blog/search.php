7266c29c624efe13ca43dfabf3e5cdbd569d0dc8
Adachi Masakazu <marscode48@gmail.com>
2022-01-21 08:26:55 +0900

    searchの投稿をroopで共通化

--- wp-content/themes/blog/search.php
+++ wp-content/themes/blog/search.php
@@ -11,35 +11,9 @@
           「<?php echo esc_html($_GET['s']); ?>」の検索結果：<?php echo $wp_query->found_posts; ?>件
         </h1>
 
-        <?php while (have_posts()) : the_post(); ?>
-          <?php
-            $cat = get_the_category();
-            $catname = $cat[0]->cat_name;
-          ?>
-          <article class="main__article">
-            <h2 class="main__article-title">
-              <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </h2>
-            <ul class="main__list">
-              <li><?php the_time('Y/m/d'); ?></li>
-              <li><?php echo $catname; ?></li>
-            </ul>
-            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-            <div class="main__text">
-              <?php
-                if (mb_strlen(strip_tags(get_the_content()), 'UTF-8') > 80) {
-                  $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'UTF-8');
-                  echo $content . '…';
-                } else {
-                  echo strip_tags(get_the_content());
-                }
-              ?>
-            </div>
-            <div class="readmore"><a href="<?php the_permalink(); ?>">READ MORE</a></div>
-          </article>
-        <?php endwhile; ?>
-
         <?php
+          get_template_part('loop');
+
           if (function_exists("pagination")) {
             pagination($wp_query->max_num_pages);
           }
@@ -50,7 +24,7 @@
 
     <?php endif; ?>
   </main>
-  
+
   <?php get_sidebar(); ?>
 </div>
 
