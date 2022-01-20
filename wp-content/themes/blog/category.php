d9ba9499bca27b1bec92bc3d34aba9241c2cef05
Adachi Masakazu <marscode48@gmail.com>
2022-01-21 08:26:34 +0900

    categoryの投稿をroopで共通化

--- wp-content/themes/blog/category.php
+++ wp-content/themes/blog/category.php
@@ -8,36 +8,15 @@
     ?>
     <h1 class="main__page-title"><?php echo $catname; ?>一覧</h1>
 
-    <?php if (have_posts()) : ?>
-      <?php while (have_posts()) : the_post(); ?>
-        <article class="main__article">
-          <h2 class="main__article-title">
-            <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-          </h2>
-          <ul class="main__list">
-            <li><?php the_time('Y/m/d'); ?></li>
-          </ul>
-          <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a>
-          <div class="main__text">
-            <?php
-            if (mb_strlen(strip_tags(get_the_content()), 'UTF-8') > 80) {
-              $content = mb_substr(strip_tags(get_the_content()), 0, 80, 'UTF-8');
-              echo $content . '…';
-            } else {
-              echo strip_tags(get_the_content());
-            }
-            ?>
-          </div>
-          <div class="readmore"><a href="<?php the_permalink(); ?>">READ MORE</a></div>
-        </article>
-      <?php endwhile; ?>
+    <?php
+      if (have_posts()):
+        get_template_part('loop');
 
-      <?php
         if (function_exists("pagination")) {
           pagination($wp_query->max_num_pages);
         }
-      ?>
-    <?php endif; ?>
+      endif;
+    ?>
   </main>
 
   <?php get_sidebar(); ?>
